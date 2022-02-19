fc19fc3fb4a6825d7a2e8e992677c593e185cafe
gnoparus <surapong@gmail.com>
2022-02-19 22:38:20 +0700

    Fixed typo.

--- contracts/test/MockDAI.sol
+++ contracts/test/MockDAI.sol
@@ -1,9 +1,7 @@
-pragma solidity ^0.8.0
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockWDAI is ERC20 {
-    contructor () ERC20("Mock DAI", "DAI") {
-        
-    }
+    constructor() ERC20("Mock DAI", "DAI") {}
 }
