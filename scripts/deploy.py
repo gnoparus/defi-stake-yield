020a60c8ea9e2fa8166c07d3ed56d28a1201b214
gnoparus <surapong@gmail.com>
2022-02-20 15:17:23 +0700

    Deployed all tokens.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -9,12 +9,6 @@ from web3 import Web3
 
 KEPT_BALANCE = Web3.toWei(1000, "ether")
 
-dict_of_allowed_token = {
-    "dapp_token": get_contract("dapp_token"),
-    "fau_token": get_contract("fau_token"),
-    "weth_token": get_contract("weth_token"),
-}
-
 
 def deploy_dapp_token():
     account = get_account()
@@ -27,29 +21,36 @@ def deploy_dapp_token():
 
 def deploy_token_farm(_dapp_token):
     account = get_account()
+    dapp_token = _dapp_token
     token_farm = TokenFarm.deploy(
         _dapp_token,
         {"from": account},
         publish_source=config["networks"][network.show_active()]["verify"],
     )
 
+    ### Transfer most of token to token_farm
     tx = _dapp_token.transfer(
         token_farm.address,
         _dapp_token.totalSupply() - KEPT_BALANCE,
         {"from": account},
     )
     tx.wait(1)
-    return token_farm
+
+    ## dapp_token, weth_token, fau_token/dai
+    weth_token = get_contract("weth_token")
+    fau_token = get_contract("fau_token")
+
+    dict_of_allowed_token = {
+        dapp_token: get_contract("dai_usd_price_feed"),
+        fau_token: get_contract("dai_usd_price_feed"),
+        weth_token: get_contract("eth_usd_price_feed"),
+    }
 
 
-def add_allowed_tokens(token_farm, dict_of_allowed_token):
-    account = get_account()
-    for key in dict_of_allowed_token:
-        # print(key)
-        token_farm.addAllowedToken(dict_of_allowed_token[key])
+def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
+    pass
 
 
 def main():
     dapp_token = deploy_dapp_token()
     token_farm = deploy_token_farm(dapp_token)
-    add_allowed_tokens(token_farm, dict_of_allowed_token=dict_of_allowed_token)
