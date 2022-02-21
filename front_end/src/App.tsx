ac34b175374fe60a677af69164c8dbaf3c293b66
gnoparus <surapong@gmail.com>
2022-02-22 00:05:47 +0700

    Imported main component.

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { DAppProvider, ChainId } from "@usedapp/core";
 import { Header } from "./components/Header"
+import { Main } from './components/Main';
+
+
 function App() {
   return (
     <DAppProvider config={{
@@ -10,6 +13,7 @@ function App() {
         <Header />
         Hi!!!
       </div>
+      <Main />
     </DAppProvider>
 
   );
