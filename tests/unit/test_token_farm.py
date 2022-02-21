8cb5643688bad49426225249b9a89723feec375e
gnoparus <surapong@gmail.com>
2022-02-21 09:31:14 +0700

    Added test issue token.

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -3,6 +3,7 @@ from scripts.helpful_scripts import (
     get_account,
     get_contract,
     LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    INITIAL_PRICE_FEED_VALUE,
 )
 from scripts.deploy import deploy_token_farm_and_dapp_token
 import pytest
@@ -50,9 +51,34 @@ def test_stake_tokens(amount_staked):
     return token_farm, dapp_token
 
 
-def test_issue_tokens():
-    pass
+def test_issue_tokens(amount_staked):
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing.")
+
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    print(f"token_farm balance = {dapp_token.balanceOf(token_farm.address):28.0f}")
+
+    starting_balance = dapp_token.balanceOf(account.address)
+    print(f"starting_balance   = {starting_balance:28.0f}")
+    non_owner_account = get_account(1)
+
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, dapp_token.address, {"from": account})
+    print(f"Staked Tokens")
+
+    starting_balance = dapp_token.balanceOf(account.address)
+    print(f"starting_balance   = {starting_balance:28.0f}")
+
+    token_farm.issueTokens({"from": account})
+    print(f"Issued tokens")
+
+    ending_balance = dapp_token.balanceOf(account.address)
+    print(f"ending_balance     = {ending_balance:28.0f}")
+    print(f"starting_balance   = {starting_balance:28.0f}")
+    print(f"amount_staked      = {amount_staked:28.0f}")
+    assert ending_balance == starting_balance + amount_staked
 
 
 def main():
-    pass
+    test_issue_tokens(3200 * 10**18)
