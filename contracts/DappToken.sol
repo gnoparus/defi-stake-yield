b1a5472215b0ff9aa79252ea07f6cb4fafd9771f
gnoparus <surapong@gmail.com>
2022-02-21 09:29:35 +0700

    Changed to 1000.

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DappToken is ERC20 {
     constructor() public ERC20("Dapp Token", "DAPP") {
-        _mint(msg.sender, 1000000000 * 10**18);
+        _mint(msg.sender, 1000 * 10**18);
     }
 }
